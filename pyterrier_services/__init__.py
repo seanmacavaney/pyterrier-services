e02ac0d86ebbc83a1c4f3782d770e3d45314ad6e
Sean MacAvaney <sean.macavaney@gmail.com>
2025-04-15 12:33:22 +0100

    bump version

--- pyterrier_services/__init__.py
+++ pyterrier_services/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.3'
+__version__ = '0.4.0'
 
 from .core import http_error_retry, paginated_search, multi_query
 from .semantic_scholar import SemanticScholarApi, SemanticScholarRetriever
