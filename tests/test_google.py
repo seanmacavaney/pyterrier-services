83d0e73275cd456144f5c54f7740cb163d641fcb
Sean MacAvaney <sean.macavaney@gmail.com>
2025-04-15 17:20:30 +0100

    conditionally skip google test based on presence of api key

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,8 +1,10 @@
+import os
 import unittest
 import pandas as pd
 from pyterrier_services import GoogleApi
 
 class TestGoogle(unittest.TestCase):
+    @unittest.skipIf('GOOGLE_API_KEY' not in os.environ, 'GOOGLE_API_KEY not set')
     def test_retriever(self):
         google = GoogleApi()
         retr = google.retriever(num_results=15)
