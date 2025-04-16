6cd8466eb2b10ce4f8b757eb46904fdc075bd6bf
Sean MacAvaney <sean.macavaney@gmail.com>
2025-04-16 09:40:30 +0100

    improve dblp test

--- tests/test_dblp.py
+++ tests/test_dblp.py
@@ -1,13 +1,20 @@
+import pandas as pd
 import unittest
 from pyterrier_services import DblpApi
 
 class TestDblp(unittest.TestCase):
     def test_retriever(self):
         dblp = DblpApi()
-        retr = dblp.retriever(num_results=10)
-        retr.search('PyTerrier')
+        retr = dblp.retriever(num_results=5)
+        res = retr.search('PyTerrier')
+        self.assertIsInstance(res, pd.DataFrame)
+        self.assertEqual(len(res), 5)
+        self.assertEqual(set(res.columns), {'qid', 'query', 'docno', 'score', 'rank', 'title', 'type', 'authors', 'year'})
 
     def test_bibtex_loader(self):
         dblp = DblpApi()
         loader = dblp.bibtex_loader()
-        loader([{'docno': 'conf/cikm/MacdonaldTMO21'}])
+        res = loader(pd.DataFrame([{'docno': 'conf/cikm/MacdonaldTMO21'}]))
+        self.assertIsInstance(res, pd.DataFrame)
+        self.assertEqual(len(res), 1)
+        self.assertEqual(set(res.columns), {'docno', 'bibtex'})
