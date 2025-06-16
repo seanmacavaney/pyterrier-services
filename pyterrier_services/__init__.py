2345142d74e468f51b85bd127ffa1d4a264297e2
Sean MacAvaney <sean.macavaney@gmail.com>
2025-06-16 18:50:00 +0100

    missing import
    fixes #5

--- pyterrier_services/__init__.py
+++ pyterrier_services/__init__.py
@@ -3,13 +3,13 @@ __version__ = '0.4.2'
 from .core import http_error_retry, paginated_search, multi_query
 from .semantic_scholar import SemanticScholarApi, SemanticScholarRetriever
 from .pinecone import PineconeApi, PineconeSparseModel, PineconeDenseModel, PineconeReranker
-from .dblp import DblpApi, DblpRetriever
+from .dblp import DblpApi, DblpRetriever, DblpBibtexLoader
 from .google import GoogleApi, GoogleSearchRetriever
 
 __all__ = [
 	'http_error_retry', 'paginated_search', 'multi_query',
 	'SemanticScholarApi', 'SemanticScholarRetriever',
 	'PineconeApi', 'PineconeSparseModel', 'PineconeDenseModel', 'PineconeReranker',
-	'DblpApi', 'DblpRetriever',
+	'DblpApi', 'DblpRetriever', 'DblpBibtexLoader',
 	'GoogleApi', 'GoogleSearchRetriever',
 ]
